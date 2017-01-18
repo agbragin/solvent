f1a4298dcf28f454ac4f950c7338e487ea40adbe
Anton Bragin <abragin@parseq.pro>
2017-01-18 17:12:23 +0300

    Merge pull request #3 in SB/ghop-core from feature/VF-357 to master
    Squashed commit of the following:
    
    commit fdead174ff831a89a743be54af0e0f54ea7a74e1
    Author: Anton Bragin <abragin@parseq.pro>
    Date:   Wed Jan 18 16:20:56 2017 +0300
    
        README file added.
    
    commit 861c62578bc4611d934e16ea245ee344a371113f
    Author: Anton Bragin <abragin@parseq.pro>
    Date:   Wed Jan 18 15:51:01 2017 +0300
    
        Deployment to Tomcat implemented.
    
        Memory leaks checked (nothing up to date).

--- src/main/java/pro/parseq/ghop/GHopCoreApplication.java
+++ src/main/java/pro/parseq/ghop/GHopCoreApplication.java
@@ -2,12 +2,19 @@ package pro.parseq.ghop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class GHopCoreApplication {
+public class GHopCoreApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GHopCoreApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GHopCoreApplication.class, args);
 	}
