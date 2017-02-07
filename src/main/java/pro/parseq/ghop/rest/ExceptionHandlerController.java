01bf03ef0bfa584132b99108b0ac8f029b1b482c
Anton Bragin <abragin@parseq.pro>
2017-02-07 15:19:57 +0300

    Merge pull request #6 in SB/ghop-core from release/VF-371-license to development
    Squashed commit of the following:
    
    commit ee1597151d81ecf1371e3d970e19f97d92fe3e0f
    Author: Anton Bragin <abragin@parseq.pro>
    Date:   Mon Feb 6 15:55:22 2017 +0300
    
        Program name added to source license header.
    
    commit 406a9ac3278cfd5a63cc48d69f9f572b98763464
    Author: Anton Bragin <abragin@parseq.pro>
    Date:   Mon Feb 6 14:50:15 2017 +0300
    
        License changed from GNU GPL to GNU Affero GPL.
    
    commit cfcc86d6b35ec3b40d72434ab5439df7cf9eb405
    Author: Anton Bragin <abragin@parseq.pro>
    Date:   Mon Feb 6 14:43:41 2017 +0300
    
        GPLv3 license added. License header added to source files.

--- src/main/java/pro/parseq/ghop/rest/ExceptionHandlerController.java
+++ src/main/java/pro/parseq/ghop/rest/ExceptionHandlerController.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ *     Copyright 2016-2017 the original author or authors.
+ *
+ *     This file is part of CONC.
+ *
+ *     CONC. is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     CONC. is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with CONC. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package pro.parseq.ghop.rest;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
